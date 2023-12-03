c23caf8f3043ab660d976b84f2e3a8c60a1765f0
Adrian Suliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-12-03 17:35:12 +0100

    solution to ex 6

---
+++ 06 - Zadania z rekurencji/ex_06.py
@@ -0,0 +1,15 @@
+from random import randrange
+def cutT(T, index, indSum, valSum):
+    if indSum != 0 and indSum == valSum:
+        return valSum
+    if index == len(T) - 1: return -1
+    return max(cutT(T, index + 1, indSum + index + 1, valSum + T[index+1]), cutT(T, index + 1, indSum, valSum))
+
+def ex6(n):
+    T = [1, 7, 3, 5, 11, 2]
+    n = len(T)
+    #T = [randrange(1,21) for _ in range(n)]
+    return cutT(T, -1, 0, 0)
+
+#n = int(input())
+print(ex6(6))
