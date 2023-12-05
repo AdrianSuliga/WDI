5580ede9316005ba766e7f6cd665c79e9e66bef4
Adrian Suliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-12-05 23:28:56 +0100

    Final solution to exercise 9

--- 06 - Zadania z rekurencji/ex_09.py
+++ 06 - Zadania z rekurencji/ex_09.py
@@ -1,12 +1,20 @@
 from random import randrange
-def printMass(T, index, sum, target):
-    if sum == target: return True
+def printMass(T, index, sum1, sum2, target):
+    if abs(sum1 - sum2) == target: return True
     if index == len(T) - 1: return False
 
-    
+    if printMass(T, index + 1, sum1 + T[index + 1], sum2, target):
+        print("P", T[index + 1])
+        return True
+    elif printMass(T, index + 1, sum1, sum2 + T[index + 1], target):
+        print("L", T[index + 1])
+        return True
+    elif printMass(T, index + 1, sum1, sum2, target):
+        print("N")
+        return True
 
 def ex9(T, target):
-    return printMass(T, -1, 0, target) 
+    return printMass(T, -1, 0, 0, target) 
 
 n = int(input("n="))
 target = int(input("target="))
