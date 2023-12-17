297af8b282aad1951b4b457a5a8641cf1459da5a
Adrian Suliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-12-17 20:31:23 +0100

    Bit algo guardian ex

--- 07 - Struktury odsyłaczowe/BIT_ALGO.py
+++ 07 - Struktury odsyłaczowe/BIT_ALGO.py
@@ -7,9 +7,9 @@ def printList(head:Node):
     while head != None:
         print(head.val, "-> ", end='')
         head = head.next
-    print("END")
+    print("NONE")
 
-def remove(ptr:Node, val:int):
+def removeElement(ptr:Node, val:int):
     prev, first = None, ptr
     while ptr != None:
         if ptr.val == val and prev == None:
@@ -22,10 +22,25 @@ def remove(ptr:Node, val:int):
         ptr = ptr.next
     return first
 
-n1 = Node(10)
+def addElement(ptr:Node, val:int):
+    if ptr == None: return Node(val)
+    prev, first = None, ptr
+    while ptr != None:
+        prev = ptr
+        ptr = ptr.next
+    prev.next = Node(val)
+    return first
+
+n1 = Node(10, None)
 n2 = Node(34, n1)
 n3 = Node(89, n2)
 n4 = Node(51, n3)
+
 printList(n4)
-a = remove(n4, 89)
-printList(a)
+l = addElement(n4, 19)
+printList(l)
+
+
+# removing element from list
+# l = removeElement(n4, 34)
+# printList(l)
